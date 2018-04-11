857350ced448d227951ad4de5f34e45f5debb8a4
Henry Borchers <henryborchers@yahoo.com>
2018-04-11 12:36:25 -0500

    added install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     packages=['jenklint'],
     zip_safe=True,
+    install_requires=["requests"],
     entry_points={
         "console_scripts": [
             'jenklint = jenklint.__main__:main'
