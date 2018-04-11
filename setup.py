cd22e915259ee8563f8de9d9caea713a2f58fc34
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-04-11 12:09:50 -0500

    added zipsafe to true

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     packages=['jenklint'],
+    zip_safe=True,
     entry_points={
         "console_scripts": [
             'jenklint = jenklint.__main__:main'
