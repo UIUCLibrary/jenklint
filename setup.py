c0f03e890de1248574660120d94c590a5d918395
hborcher <hborcher@illinois.edu>
2019-10-17 12:06:19 -0500

    require requests>=2.20.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     packages=['jenklint'],
     zip_safe=True,
-    install_requires=["requests"],
+    install_requires=["requests>=2.20.0"],
     entry_points={
         "console_scripts": [
             'jenklint = jenklint.__main__:main'
